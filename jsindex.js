300b76d94aeee80571f6177509a5db5bd572d332
JGLoot <146401997+JGLoot@users.noreply.github.com>
2024-01-30 19:10:03 -0300

    Update jsindex.js

--- jsindex.js
+++ jsindex.js
@@ -177,7 +177,7 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000); 
+  setTimeout(showSlides, 6000); 
 }
 
 //////////////////////////////////////////////////// BUSCAR PRODUTOS /////////////////////////////////////////////////////////////////////
