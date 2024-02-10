6d17ee9b996a10c3fbe844dba6dc2acd246d84b4
JGLoot <146401997+JGLoot@users.noreply.github.com>
2024-02-10 12:35:57 -0300

    Atualizar o jsprodutos.js

--- jsprodutos.js
+++ jsprodutos.js
@@ -122,7 +122,7 @@ function calcularPrecoTotal(produto) {
 function calcularPrecoTotalCarrinho() {
   var carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
   var precoTotalCarrinho = carrinho.reduce(function (total, produto) {
-    var quantidadeConsiderada = Math.min(produto.quantidade, 1);
+    var quantidadeConsiderada = Math.min(produto.quantidade);
     return total + quantidadeConsiderada * produto.preco;
   }, 0);
   var precoTotalElement = document.getElementById('precoTotalCarrinho');
